c8ff7082ddf917648c1d4e2a3bec6351642863ea
rob jahn <robjahn@gmail.com>
2019-07-03 09:52:47 -0400

    adjust help output

--- buildpush.sh
+++ buildpush.sh
@@ -7,7 +7,7 @@ export IMAGE=$GITHUB_ACCOUNT/keptn-orders-front-end:$VERSION_TAG
 clear
 if [ $# -lt 2 ]
 then
-  echo "missing arguments. Expect ./quickbuild.sh <registry> <tag>"
+  echo "missing arguments. Expect ./buildpush.sh <registry> <tag>"
   exit 1
 fi
 
